bb56f8c6c9be367aa32c8123acb1ab6b2b680ad0
iloveyou416068 <iloveyou416068@sina.com>
2017-09-14 08:29:22 +0800

    update node

--- build/build_in_container.sh
+++ build/build_in_container.sh
@@ -6,7 +6,7 @@ mkdir -p dist
 
 mkdir -p api/cmd/portainer/$binary
 
-docker run --rm -tv $(pwd)/api:/src -e BUILD_GOOS="$1" -e BUILD_GOARCH="$2" portainer/golang-builder:cross-platform /src/cmd/portainer
+docker run -tv  $(pwd)/api:/src -e BUILD_GOOS="$1" -e BUILD_GOARCH="$2" portainer/golang-builder:cross-platform /src/cmd/portainer
 
 mv "api/cmd/portainer/$binary" dist/
 #sha256sum "dist/$binary" > portainer-checksum.txt
